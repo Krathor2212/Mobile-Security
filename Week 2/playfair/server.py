37740eec4fa9709e136ab9a8637a6e66b9fb3e96
Krathor2212 <phome1625@gmail.com>
2025-07-22 00:26:35 +0530

    Update server.py

--- Week 2/playfair/server.py
+++ Week 2/playfair/server.py
@@ -8,8 +8,6 @@ server.listen()
 message = ''
 conn,addr = server.accept()
 print("Request accepted from "+str(addr))
-
-
 while message != 'q':
     message = conn.recv(1024).decode()
     if not message:
@@ -19,20 +17,15 @@ while message != 'q':
         message,key = t[0],t[1]
         decrypt_message = decrypt(message,key)
         print(f'Client: {decrypt_message}')
-
         if message == 'q':
             break
-
         message = input("Enter Server Message: ")
         if message == 'q':
             break
         key = input("Enter Server Key: ")
         print(f'Server : {message}')
-        
-
         encrypt_message = encrypt(message,key)
         tosend_message = f'{encrypt_message}:{key}'
         conn.send(tosend_message.encode())
-        
 conn.close()
     
